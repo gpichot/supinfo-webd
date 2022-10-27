778f47bb83e7f22f1788fee82fbfb3e397ffc857
Gabriel Pichot <gabriel.pichot@gmail.com>
2022-10-27 09:36:27 +0200

    feat: add fruits

---
+++ jour3/fruits.js
@@ -0,0 +1,11 @@
+const fruits = ["pomme", "poire", "fraise", "abricot"];
+
+function afficherFruit(fruit) {
+  console.log(`J'ai mangé un.e ${fruit}`);
+}
+
+afficherFruit(fruits[0]);
+
+// fruits.forEach();
+
+console.log("Version itérative");
